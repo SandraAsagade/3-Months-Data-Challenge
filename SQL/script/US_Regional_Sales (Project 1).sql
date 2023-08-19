1451be5817830d6140666a6dd7dcb897093207a3
Sandra Asagade <86619549+SandraAsagade@users.noreply.github.com>
2023-08-19 19:01:36 +0100

    Update US_Regional_Sales (Project 1).sql

--- SQL/script/US_Regional_Sales (Project 1).sql
+++ SQL/script/US_Regional_Sales (Project 1).sql
@@ -1,6 +1,6 @@
 /* US REGIONAL SALES (FICTIONAL SALES DATA) FOR SQL PROJECT */
 
--- Checking for duplicates
+--1. Checking for duplicates
 USE usregionalsales;
 SELECT
 	OrderNumber,
@@ -9,7 +9,7 @@ FROM sales_order
 GROUP BY OrderNumber
 HAVING COUNT(OrderNumber) > 1;
 
--- Adding a Revenue and Profit column
+--2. Adding a Revenue and Profit column
 ALTER TABLE sales_order ADD Revenue DOUBLE;
 UPDATE sales_order
 SET Revenue = ROUND((UnitPrice * OrderQuantity),2);
@@ -19,7 +19,7 @@ UPDATE sales_order
 SET Profit = ROUND(((UnitPrice - UnitCost)*OrderQuantity),2);
 SELECT * FROM sales_order;
 
--- converting and changing the date format from text(dd/mm/yyyy) to date(yyyy-mm-dd) 
+--3. converting and changing the date format from text(dd/mm/yyyy) to date(yyyy-mm-dd) 
 UPDATE usregionalsales.sales_order
 SET ProcuredDate = str_to_date(ProcuredDate, "%d/%m/%Y");
 UPDATE usregionalsales.sales_order
@@ -35,7 +35,7 @@ SELECT
     DeliveryDate 
 FROM sales_order;
 
--- Total transactions made
+--4. Total transactions made
 SELECT
 	COUNT(*) AS Total_orders_made,
     SUM(OrderQuantity) AS Total_Quantity,
@@ -43,7 +43,7 @@ SELECT
     ROUND(SUM(Profit),2) AS Total_Profit
 FROM sales_order;
 
--- Number of transactions/products/total revenue/profit ordered per order date(year)
+--5. Number of transactions/products/total revenue/profit ordered per order date(year)
 SELECT
 	DATE_FORMAT(OrderDate, '%Y') AS Year,
     COUNT(*) AS Total_Transaction,
@@ -54,27 +54,27 @@ FROM sales_order
 GROUP BY Year
 ORDER BY Revenue DESC;
 
--- Transaction per Sales channel
+--6. Transaction per Sales channel
 SELECT
 	SalesChannel,
     COUNT(*) AS Total_Transaction
 FROM sales_order
 GROUP BY SalesChannel;
 
--- How many customers ordered
+--7. How many customers ordered
 SELECT
     COUNT(DISTINCT _CustomerID) AS Total_Customers,
     COUNT(DISTINCT _SalesTeamID) AS Total_Sales_Team
 FROM sales_order s1;
 
--- Distinct Products ordered
+--8. Distinct Products ordered
 SELECT
     COUNT(DISTINCT s1._ProductID) AS Total_Products
 FROM sales_order s1
 JOIN product p
 	USING(_ProductID);
 
--- Total number of Orders each customer made
+--9. Total number of Orders each customer made
 SELECT
 	c._CustomerID,
     CustomerNames,
@@ -85,7 +85,7 @@ JOIN customer c
 GROUP BY _CustomerID
 ORDER BY Total_Transaction DESC;
 
--- Total number of stores in each region
+--10. Total number of stores in each region
 SELECT
 	Region,
     COUNT(_StoreID) AS Stores_Count
@@ -95,7 +95,7 @@ JOIN region
 GROUP BY Region
 ORDER BY Stores_Count DESC;
 
--- Total state in each region
+--11. Total state in each region
 SELECT
 	Region,
 	COUNT(State) AS Total_States
@@ -103,7 +103,7 @@ FROM region
 GROUP BY Region
 ORDER BY Total_States DESC;
 
--- Total quantity sold and transaction per store
+--12. Total quantity sold and transaction per store
 SELECT
 	_StoreID,
     COUNT(*) AS Transaction_Count,
@@ -112,7 +112,7 @@ FROM sales_order
 GROUP BY _StoreID
 ORDER BY Total_Quantity_Sold DESC;
 
--- Total revenue and profit from each city
+--13. Total revenue and profit from each city
 SELECT
 	CityName,
      ROUND(SUM(Revenue),2) AS Total_Revenue,
@@ -123,7 +123,7 @@ JOIN location l
 GROUP BY l.CityName
 ORDER BY Revenue DESC;
 
--- Most expensive and least expensive products
+--14. Most expensive and least expensive products
 SELECT
 	p.ProductName,
     s1.UnitPrice AS Price,
@@ -135,7 +135,7 @@ JOIN product p
 	USING(_ProductID)
 GROUP BY p.ProductName;
 
--- Total Revenue/Profit in each region by year
+--15. Total Revenue/Profit in each region by year
 WITH cte AS (
 	SELECT
 		Region,
@@ -150,7 +150,7 @@ WITH cte AS (
 	GROUP BY Region, Year
 	ORDER BY Region, Year, Revenue)
 SELECT
-	Region,
+    Region,
     Year,
     Total_Revenue,
     Total_Profit,
@@ -160,7 +160,7 @@ FROM cte
 GROUP BY Region, Year
 ORDER BY Region;
     
--- Total Transaction per location type
+--16. Total Transaction per location type
 SELECT
 	Type,
     COUNT(*) AS Total_Transaction
@@ -170,9 +170,9 @@ JOIN location
 GROUP BY Type
 ORDER BY COUNT(*) DESC;
 
--- Sales team Performance Statistics
+--17. Sales team Performance Statistics
 SELECT
-	s2._SalesTeamID,
+    s2._SalesTeamID,
     SalesTeam,
     COUNT(*) AS Total_Transaction,
     SUM(OrderQuantity) AS Total_Quantity_Sold,
@@ -184,7 +184,7 @@ JOIN sales_team s2
 GROUP BY SalesTeam
 ORDER BY Total_Transaction DESC;
 
--- Procedure to view the stats of sales team: This shows all orders made under a particular sales team
+--18. Procedure to view the stats of sales team: This shows all orders made under a particular sales team
 USE usregionalsales;
 DROP procedure IF EXISTS sales_team_stats;
 DELIMITER $$
@@ -205,7 +205,7 @@ DELIMITER ;
 CALL sales_team_stats("Joshua Ryan");
 
 
--- Date difference between Order date and delivery date
+--19. Date difference between Order date and delivery date
 WITH cte AS(
 	SELECT
 		DATEDIFF(DeliveryDate,OrderDate) AS Delivery_Duration
